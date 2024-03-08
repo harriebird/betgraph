f796e3a8d7797bee709be9398cb9679b0a86f70e
Paul Harriet Asiñero <harriebird@yahoo.com>
2024-03-09 06:35:00 +0800

    WIP: daisyui and models.py update

--- backend/betgraph/webapp/models.py
+++ backend/betgraph/webapp/models.py
@@ -1,5 +1,6 @@
-from neomodel import (StructuredNode, StructuredRel, StringProperty, IntegerProperty, BooleanProperty,
-                      DateTimeProperty, UniqueIdProperty, UniqueProperty, RelationshipTo)
+from neomodel import (StructuredNode, StructuredRel, StringProperty,
+                      IntegerProperty, BooleanProperty, DateTimeProperty,
+                      UniqueIdProperty, UniqueProperty, RelationshipTo)
 
 
 class BetRel(StructuredRel):
@@ -12,5 +13,5 @@ class Person(StructuredNode):
     name = StringProperty(unique_index=True, required=True)
     gender = StringProperty()
     bets = RelationshipTo('Person', 'BET', model=BetRel)
-    answered = BooleanProperty(default=False)
+    answered = DateTimeProperty(default=None)
     timestamp = DateTimeProperty(default_now=True)
